b0e292a32b7113c0412a99640f7436dcaa0c260d
ADI-N <rabbritva@gmail.com>
2026-05-20 09:49:08 +0500

    feat: insort reverse

--- sort_algorithms/insertion_sort.py
+++ sort_algorithms/insertion_sort.py
@@ -1,12 +1,21 @@
-def insertion_sort(l:list):
+def insertion_sort(l:list, reverse=False):
     """Algorithm insertion_sort:\n
     Sorted input massive\n
-    Example: input: [5,2,4,6,1,3] —> output: [1, 2, 3, 4, 5, 6]
+    Example: input: [2,5,4,6,1,3] —> output: [1, 2, 3, 4, 5, 6]
     """
-    for j in range(1, len(l)):
-        x = l[j]
-        i = j-1
-        while i>=0 and l[i]>x:
-            l[i+1] = l[i]
-            i-=1
-        l[i + 1] = x
+    if reverse:
+        for j in range(1, len(l)):
+            x = l[j]
+            i = j-1
+            while i>=0 and l[i]<x:
+                l[i+1] = l[i]
+                i -= 1
+            l[i+1] = x
+    else:
+        for j in range(1, len(l)):
+            x = l[j]
+            i = j-1
+            while i>=0 and l[i]>x:
+                l[i+1] = l[i]
+                i-=1
+            l[i + 1] = x
